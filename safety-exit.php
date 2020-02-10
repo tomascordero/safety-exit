34c588e26351db9df6243e0303e435e972aa538f
Tomas Cordero <tcorderoiv@gmail.com>
2020-02-10 11:05:10 -0700

    hot fix for error handline

--- safety-exit.php
+++ safety-exit.php
@@ -20,20 +20,40 @@
  */
 
 
+
+$errors = false;
+// Check to see if required file exists. If not do not initialize plugin
+try {
+	if( !file_exists(realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php") ) {
+		throw new Exception ('Unable to load pluggable.php in: ' . realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php" . ' Safety Exit is disabled until this error is fixed.');
+	}else{
+		require_once(realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php");
+	}
+}catch(Exception $e){
+	if ( is_admin() ){
+	?>
+	<div class="error notice">
+		<p><?= $e->getMessage(); ?></p>
+	</div>
+	<?php
+	}
+	$errors = true;
+}
 // If this file is called directly, abort.
-require_once(realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php");
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 foreach ( glob( plugin_dir_path( __FILE__ ) . 'lib/*.php' ) as $file ) {
     include_once $file;
 }
-if ( is_admin() ){
-	if(current_user_can('administrator')){
-		$admin = new Safety_Exit_Admin(__FILE__);
-		$admin->init();
+if( !$errors ) {
+	if ( is_admin() ){
+		if(current_user_can('administrator')){
+			$admin = new Safety_Exit_Admin(__FILE__);
+			$admin->init();
+		}
+	} else {
+		$frontend = new Safety_Exit_Frontend(__FILE__);
+		$frontend->init();
 	}
-} else {
-	$frontend = new Safety_Exit_Frontend(__FILE__);
-	$frontend->init();
 }
