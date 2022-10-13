f3775f5272390280c71ce6298614193e872e7254
tomascordero <tcorderoiv@gmail.com>
2022-10-12 21:00:05 -0700

    Added logic for rendering fontawesome

--- lib/Safety_Exit_Frontend.php
+++ lib/Safety_Exit_Frontend.php
@@ -27,7 +27,14 @@ class Safety_Exit_Frontend {
 	public function sftExt_enqueue() {
 		wp_enqueue_style('frontendCSS', plugins_url() . '/safety-exit/assets/css/frontend.css');
 		wp_enqueue_script( 'frontendJs', plugins_url() . '/safety-exit/assets/js/frontend.js', array('jquery') );
-		wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.3.1/css/all.css' );
+
+		$args = wp_parse_args(get_option('sftExt_settings'), array(
+        	'sftExt_rectangle_icon_onOff' => 'yes'
+		));
+
+		if(isset($args['sftExt_rectangle_icon_onOff']) && $args['sftExt_rectangle_icon_onOff'] !== 'no') {
+			wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.3.1/css/all.css' );
+		}
 	}
 	public function safety_exit_injectTest() {
 		include_once( 'views/frontend-button.php' );
