e18c740ccd3d0138ba6120881a8ff097d887bab8
tomascordero <tcorderoiv@gmail.com>
2022-12-23 14:47:53 -0700

    Added back in do_action

--- lib/Safety_Exit_Frontend.php
+++ lib/Safety_Exit_Frontend.php
@@ -22,6 +22,7 @@ class Safety_Exit_Frontend {
 	public function init() {
 		add_action('wp_enqueue_scripts', array($this, 'sftExt_enqueue'));
 		add_action( 'wp_body_open', array($this, 'safety_exit_inject'), 100 );
+		do_action( 'wp_body_open' );
     }
 	public function sftExt_enqueue() {
 		wp_enqueue_style('frontendCSS', plugins_url() . '/safety-exit/assets/css/frontend.css');
