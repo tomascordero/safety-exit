6dee642f72bbfdf7745b5455d4dbf9ef20df2a1f
tomascordero <tcorderoiv@gmail.com>
2023-09-12 22:44:05 -0700

    got front end button loading

--- lib/Safety_Exit_Frontend.php
+++ lib/Safety_Exit_Frontend.php
@@ -16,31 +16,11 @@ class Safety_Exit_Frontend {
 
 	private $buttonInitialized = false;
 
+	private $defaultSettings;
+
 	public function __construct($file) {
 		$this->pluginRoot = $file;
-	}
-	public function init() {
-		add_action('wp_enqueue_scripts', array($this, 'sftExt_enqueue'));
-		do_action( 'qm/debug', 'wp_enqueue_scripts fired' );
-		add_action('wp_head', array($this, 'safety_exit_custom_css'));
-		// add_action( 'wp_body_open', array($this, 'safety_exit_inject'), 100 );
-		do_action( 'qm/debug', 'wp_body_open fired' );
-    }
-	public function sftExt_enqueue() {
-		wp_enqueue_style('frontendCSS', plugins_url() . '/safety-exit/assets/css/frontend.css');
-		wp_enqueue_script( 'frontendJs', plugins_url() . '/safety-exit/assets/js/frontend.js', array('jquery') );
-
-		$args = wp_parse_args(get_option('sftExt_settings'), array(
-        	'sftExt_rectangle_icon_onOff' => 'yes'
-		));
-
-		if(isset($args['sftExt_rectangle_icon_onOff']) && $args['sftExt_rectangle_icon_onOff'] !== 'no') {
-			wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.3.1/css/all.css' );
-		}
-	}
-	public function safety_exit_custom_css() {
-		do_action( 'qm/debug', 'generating custom CSS' );
-		$sftExtSettings = wp_parse_args(get_option('sftExt_settings'), array(
+		$this->defaultSettings = wp_parse_args(get_option('sftExt_settings'), array(
 			'sftExt_position' => 'bottom right',
 			'sftExt_fontawesome_icon_classes' => 'fas fa-times',
 			'sftExt_type' => 'rectangle',
@@ -59,28 +39,71 @@ class Safety_Exit_Frontend {
 			'sftExt_front_page' => 'yes',
 			'sftExt_pages' => array()
 		));
+	}
+	public function init() {
+		add_action('wp_enqueue_scripts', array($this, 'sftExt_enqueue'));
+		do_action( 'qm/debug', 'wp_enqueue_scripts fired' );
+		add_action('wp_head', array($this, 'safety_exit_custom_styling'));
+		// add_action( 'wp_body_open', array($this, 'safety_exit_inject'), 100 );
+		do_action( 'qm/debug', 'wp_body_open fired' );
+    }
+	public function sftExt_enqueue() {
+		wp_enqueue_style('frontendCSS', plugins_url() . '/safety-exit/assets/css/frontend.css');
+		wp_enqueue_script( 'frontendJs', plugins_url() . '/safety-exit/assets/js/frontend.js', array('jquery') );
+
+		$args = wp_parse_args(get_option('sftExt_settings'), array(
+        	'sftExt_rectangle_icon_onOff' => 'yes'
+		));
+
+		if(isset($args['sftExt_rectangle_icon_onOff']) && $args['sftExt_rectangle_icon_onOff'] !== 'no') {
+			wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.3.1/css/all.css' );
+		}
+	}
+	public function safety_exit_custom_styling() {
+		do_action( 'qm/debug', 'generating custom CSS' );
+
+		$classes = $this->defaultSettings['sftExt_position'] . ' ' . $this->defaultSettings['sftExt_type'];
+		$icon = '';
+		if($this->defaultSettings['sftExt_rectangle_icon_onOff'] == 'yes' && $this->defaultSettings['sftExt_type'] == 'rectangle') {
+			$icon = '<i class="' . $this->defaultSettings['sftExt_fontawesome_icon_classes'] . '"></i>';
+		}else if($this->defaultSettings['sftExt_type'] == 'round' || $this->defaultSettings['sftExt_type'] == 'square'){
+			$icon = '<i class="' . $this->defaultSettings['sftExt_fontawesome_icon_classes'] . '"></i>';
+		}
+		$displayButton = true;
+		if($this->defaultSettings['sftExt_show_all'] == 'no'){
+			if( !in_array(get_the_ID(), $this->defaultSettings['sftExt_pages'])){
+				$displayButton = false;
+			}
+		}
 		$hideOnMobile = false;
-		if($sftExtSettings['sftExt_hide_mobile'] == 'yes') {
+		if($this->defaultSettings['sftExt_hide_mobile'] == 'yes') {
 			$hideOnMobile = true;
 		}
-		// TODO: Keep migrating stuff
+
+		// if is_front_page() and "show on front page option is selected"
+		if($this->defaultSettings['sftExt_front_page'] == 'yes' && is_front_page()){
+			$displayButton = true;
+		}
 		?>
+		<script>
+			window.sftExtBtn =  {};
+			window.sftExtBtn.classes = '<?= $classes ?>';
+			window.sftExtBtn.icon = '<?= $icon ?>';
+			window.sftExtBtn.newTabUrl = "<?= $this->defaultSettings['sftExt_new_tab_url'] ?>";
+			window.sftExtBtn.currentTabUrl = "<?= $this->defaultSettings['sftExt_current_tab_url']?>";
+			window.sftExtBtn.btnType = "<?= $this->defaultSettings['sftExt_type'] ?>";
+			window.sftExtBtn.text = "<?= $this->defaultSettings['sftExt_rectangle_text'] ?>";
+		</script>
 		<style>
-			#sftExt-frontend-button {
-				background-color: <?= $sftExtSettings['sftExt_bg_color'] ?>;
-				color: <?= $sftExtSettings['sftExt_font_color'] ?>;
-			}
-			<?php if($hideOnMobile) : ?>
-			@media (max-width: 600px) {
-				#sftExt-frontend-button {
-					display: none !important;
-				}
-			}
-			<?php endif; ?>
-			#sftExt-frontend-button.rectangle {
-				font-size: <?= $sftExtSettings['sftExt_rectangle_font_size'] . $sftExtSettings['sftExt_rectangle_font_size_units']  ; ?>;
-				letter-spacing: <?= $sftExtSettings['sftExt_letter_spacing']; ?>;
-				border-radius: <?= $sftExtSettings['sftExt_border_radius']; ?>px;
+			:root {
+				--sftExt_bgColor: <?= $this->defaultSettings['sftExt_bg_color'] ?>;
+				--sftExt_textColor: <?= $this->defaultSettings['sftExt_font_color'] ?>;
+				--sftExt_active: <?= !$displayButton ? 'none !important' : 'inline-block' ?>;
+				--sftExt_activeMobile: <?= $hideOnMobile ? 'none !important' : 'inline-block' ?>;
+				--sftExt_mobileBreakPoint: 600px;
+				--sftExt_rectangle_fontSize: <?= $this->defaultSettings['sftExt_rectangle_font_size'] . $this->defaultSettings['sftExt_rectangle_font_size_units'] ?>;
+				--sftExt_rectangle_letterSpacing: <?= $this->defaultSettings['sftExt_letter_spacing'] ?>;
+				--sftExt_rectangle_borderRadius: <?= $this->defaultSettings['sftExt_border_radius']; ?>px;
 			}
 		</style>
 		<?php
