e14500d569bf0f22ac674c7830772ea6594f643c
tomascordero <tcorderoiv@gmail.com>
2023-09-15 11:21:59 -0700

    Started working on settings view

--- lib/views/global-settings.php
+++ lib/views/global-settings.php
@@ -1,10 +1,12 @@
 <div class="wrap">
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-	<form method="post" action="options.php">
+
+	<div id="sftExtApp"></div>
+	<!-- <form method="post" action="options.php">
 		<?php
-			settings_fields( 'pluginPage' );
-			do_settings_sections( 'pluginPage' );
-			submit_button();
+			// settings_fields( 'pluginPage' );
+			// do_settings_sections( 'pluginPage' );
+			// submit_button();
 		?>
-    </form>
+    </form> -->
 </div>
